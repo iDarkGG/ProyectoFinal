a2f592ee0c7e3bf42da060e049cb2f47977a7db3
iDarkGG <harryriva14@gmail.com>
2022-10-06 09:09:00 -0600

    Esquema de Clases v0.5

---
+++ WinFormsApp1/PaginaPrincipal.cs
@@ -0,0 +1,10 @@
+namespace WinFormsApp1
+{
+    public partial class PaginaPrincipal : Form
+    {
+        public PaginaPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
